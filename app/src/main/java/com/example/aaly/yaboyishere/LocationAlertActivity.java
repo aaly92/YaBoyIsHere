2aea22c3b29068f915478278e5733edf6c2b787a
Ahmed Aly <ahmaly01@gmail.com>
2016-06-09 10:26:25 -0400

    Initial commit - Uploading basic project that android studio initializes

---
+++ app/src/main/java/com/example/aaly/yaboyishere/LocationAlertActivity.java
@@ -0,0 +1,13 @@
+package com.example.aaly.yaboyishere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocationAlertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
