5bfea3edb5067e6d1ee65ec28d67c424ce230ce5
Ahmed Aly <ahmaly01@gmail.com>
2016-06-09 14:37:13 -0400

    added a username when posting to slack

--- app/src/main/java/com/example/aaly/yaboyishere/LocationService.java
+++ app/src/main/java/com/example/aaly/yaboyishere/LocationService.java
@@ -77,10 +77,10 @@ public class LocationService extends Service {
             float distance = location.distanceTo(pointLocation);
             SlackPost slackPost = null;
             if (distance <= POINT_RADIUS && !currentLocation.equals("INSIDE")) {
-                slackPost = new SlackPost("Hov's in the building");
+                slackPost = new SlackPost("Hov's in the building", "HOVA");
                 currentLocation = "INSIDE";
             } else if (distance > POINT_RADIUS && !currentLocation.equals("OUTSIDE")) {
-                slackPost = new SlackPost("Hov's out the building");
+                slackPost = new SlackPost("Hov's out the building", "HOVA");
                 currentLocation = "OUTSIDE";
             }
             if (slackPost != null) {
--- app/src/main/java/com/example/aaly/yaboyishere/SlackPost.java
+++ app/src/main/java/com/example/aaly/yaboyishere/SlackPost.java
@@ -6,16 +6,26 @@ package com.example.aaly.yaboyishere;
 public class SlackPost {
 
     private String text;
+    private String username;
 
-    public SlackPost(String text) {
+    public SlackPost(String text, String username) {
         this.text = text;
+        this.username = username;
     }
 
     public String getText() {
         return text;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
     public void setText(String text) {
         this.text = text;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
